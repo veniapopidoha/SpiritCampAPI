437dc03b2e8987c559538dad398bc8c316f3db73
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 17:54:29 +0300

    fix signature

--- router.js
+++ router.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
 router.post('/paid', async (req, res) => {
     var sign = LiqPay.str_to_sign(
         'sandbox_PJWkYmAbiaUeS0oMAZJzumjlPifRWcWzC1A70N2e' +
-        data +
+        req.data +
         'sandbox_i7113317942'
     );
     console.log('Signature from server - ', req.body.signature);
