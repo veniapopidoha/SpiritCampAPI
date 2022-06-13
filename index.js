5d4be815968a2e89734823dc873225e48022a478
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 12:37:21 +0300

    Fix port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./router.js')
+const router = require('./router.js');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 mongoose.connect("mongodb+srv://admin:1@cluster0.gpxeh.mongodb.net/?retryWrites=true&w=majority");
@@ -9,6 +10,6 @@ app.use(express.json());
 
 app.use(router)
 
-app.listen(5000, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server Started at ${5000}`)
 });
