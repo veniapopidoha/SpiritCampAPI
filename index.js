bf4c0d77245ef82273ddabcc4c4b1b8f39a775b8
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 11:48:59 +0300

    HTTP port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use(router)
 
-app.listen(5000, () => {
-    console.log(`Server Started at ${5000}`)
+app.listen(80, () => {
+    console.log(`Server Started at ${80}`)
 })
