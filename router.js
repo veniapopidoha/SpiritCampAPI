e321755b99439e1e24df277d90925e13dfa7c718
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 11:33:27 +0300

    Fix router

--- router.js
+++ router.js
@@ -2,17 +2,19 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../SpiritCampAPI/Post.js')
 
-router.get('/', (req, res) => {
-    res.status(200).json(Post.find())
+router.get('/', async (req, res) => {
+    const databaseResult = await Post.find({});
+    res.status(200).json(databaseResult); 
 })
 
-router.post('/', (req, res) => {
-    Post.create(req.body)
-    res.status(200)
+router.post('/', async (req, res) => {
+    await Post.create(req.body)
+    res.status(200);
 })
 
-router.delete('/:id', (req, res) => {
-    Post.findByIdAndDelete(id)
+router.delete('/:id', async (req, res) => {
+    await Post.findByIdAndDelete(id)
+    res.status(200);
 })
 
 module.exports = router;
