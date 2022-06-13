238c5161ce9ef43e27bfd2268db688fcfa5809c5
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 11:45:10 +0300

    Fix delete

--- router.js
+++ router.js
@@ -8,13 +8,13 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    await Post.create(req.body)
-    res.status(200);
+    const createdUser = await Post.create(req.body)
+    res.status(200).json(createdUser);
 })
 
-router.delete('/:id', async (req, res) => {
-    await Post.findByIdAndDelete(id)
-    res.status(200);
+router.delete('/:id', async ({ params }, res) => {
+    const deletedUser = await Post.findByIdAndDelete(params.id)
+    res.status(200).json(deletedUser);
 })
 
 module.exports = router;
