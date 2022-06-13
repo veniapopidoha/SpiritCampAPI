7c40b6b7629c54dc53ecf58aade0e51d8a657042
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 14:22:48 +0300

    Return paid info

--- router.js
+++ router.js
@@ -14,8 +14,7 @@ router.post('/', async (req, res) => {
 
 router.post('/paid', async (req, res) => {
     console.log('Paid info - ', req.body);
-    const createdUser = await Post.create(req.body)
-    res.status(200).json(createdUser);
+    res.status(200).json(req.body);
 })
 
 router.delete('/:id', async ({ params }, res) => {
