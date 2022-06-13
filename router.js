e084b76bb44daacd7d61341283caf1196d3143b4
volodymyr_shorinov <pantene742@gmail.com>
2022-06-13 16:57:07 +0300

    Show decoded data

--- router.js
+++ router.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const Post = require('./Post.js')
+const express = require('express');
+const router = express.Router();
+const Post = require('./Post.js');
+
+var LiqPay = require('liqpay');
+
+var liqpay = new LiqPay(public_key, private_key);
+var sign = liqpay.str_to_sign(
+'sandbox_PJWkYmAbiaUeS0oMAZJzumjlPifRWcWzC1A70N2e' +
+data +
+'sandbox_i7113317942'
+);
 
 router.get('/', async (req, res) => {
     const databaseResult = await Post.find({});
@@ -14,8 +23,8 @@ router.post('/', async (req, res) => {
 
 router.post('/paid', async (req, res) => {
     console.log('Paid info paramsss - ', req.headers);
-    console.log('Paid info body - ', req.body);
-    res.status(200).json({ adwjk: "awd", body: req.body });
+    console.log('Paid info body - ', base64_decode(req.body.data));
+    res.status(200).json({ ok: true });
 });
 
 router.delete('/:id', async ({ params }, res) => {
